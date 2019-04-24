6b1da7fcf1d6266e5fe9b22b3e85e82641ac932e
choidaye <soar_dy@naver.com>
2019-04-24 19:29:31 +0900

    cardview padding

--- cardview/app/src/main/java/com/choidaye/cardview/MainActivity.kt
+++ cardview/app/src/main/java/com/choidaye/cardview/MainActivity.kt
@@ -2,18 +2,21 @@ package com.choidaye.cardview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.choidaye.cardview.Fragment.ViewPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter:ViewPagerAdapter
+    lateinit var adapter: ViewPagerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         adapter = ViewPagerAdapter(supportFragmentManager)
-        
+        ac_main_vp.adapter = adapter
+        ac_main_vp.setPadding(50,0,50,0)
+        ac_main_vp.setPageMargin(20)
+
     }
 
 
